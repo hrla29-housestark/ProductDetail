f3f79c9f81b2c0469f16a29b71130e4f8b2725cc
Jeff Cho <jeffcho0227@gmail.com>
2019-05-15 23:02:24 -0700

    [feat] will render description or specifications base on click/active

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import Style from './App.css';
+import Description from './Description.jsx';
+import Specifications from './Specifications.jsx';
 
 class App extends React.Component{
   constructor() {
     super();
+    this.state = {
+      views: 'description'
+    };
+    this.changeView = this.changeView.bind(this);
+    this.renderViews = this.renderViews.bind(this);
   }
+
+  changeView(option) {
+    this.setState({
+      views: option
+    });
+  }
+
+  renderViews() {
+    if (this.state.views === 'description') {
+      return <Description />;
+    } else if (this.state.views === 'specifications') {
+      return <Specifications />
+    }
+  }
+
+ 
   render() {
     return(
       <div className={Style.pdMain}>
         <div>PRODUCT DETAILS</div>
           <ul>
-            <li className={Style.pdLi}>
-              <button className={Style.pdBtn}>DESCRIPTION</button>
+            <li className={this.state.views === 'description' ? Style.pdLiActive : Style.pdLi} 
+                onClick={() => {this.changeView('description')}}>
+              <button className={this.state.views === 'description' ? Style.pdBtnActive : Style.pdBtn}>DESCRIPTION</button>
             </li>
-            <li className={Style.pdLi}>
-              <button className={Style.pdBtn}>SPECIFICATION</button>
+            <li className={this.state.views === 'specifications' ? Style.pdLiActive : Style.pdLi}  
+                onClick={() => {this.changeView('specifications')}}>
+              <button className={this.state.views === 'specifications' ? Style.pdBtnActive : Style.pdBtn}>SPECIFICATION</button>
             </li>
           </ul>
+          <div>
+            {this.renderViews()}
+          </div>
       </div>
     )
   }
---
+++ client/src/components/Specifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Specifications = () => {
+  return(
+    <div>
+      this is specifications
+    </div>
+  )
+};
+
+export default Specifications;
