a6d8295553e48253f649d65a91b6ecfba9b37f05
Jeff Cho <jeffcho0227@gmail.com>
2019-05-29 11:23:33 -0700

    [fix] added className for the YouMayAlsoLikeEntry images

--- client/src/components/youMayAlsoLike/YouMayAlsoLikeListEntry.jsx
+++ client/src/components/youMayAlsoLike/YouMayAlsoLikeListEntry.jsx
@@ -22,7 +22,7 @@ class YouMayAlsoLikeListListEntry extends React.Component{
       <div className={Style.product_card_container}>
         <div className={Style.product_card }>
           <span className={Style.img_wrap}>
-            <img src={this.props.item.imageUrl}/>
+            <img className={Style.img_indi} src={this.props.item.imageUrl}/>
             {!this.state.likeStatus ? 
               <svg className={Style.img_heart} onClick={(e) => this.handleLikeStatus(e)} id="wishlist-inactive" viewBox="0 0 20 24" width="7%" height="100%"><path fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2" d="M7.38 6H4.42L2 10l8 8 8-8-2.41-4h-2.98L10 9 7.38 6z"></path></svg> :
               <svg className={Style.img_heart} onClick={(e) => this.handleLikeStatus(e)} id="wishlist-active" viewBox="0 0 20 24" width="7%" height="100%"><path fill="currentColor" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2" d="M7.38 6H4.42L2 10l8 8 8-8-2.41-4h-2.98L10 9 7.38 6z"></path></svg>
